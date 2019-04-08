e7b185f1f87c9672a4b2ff1f973a7f90aa0e7129
Spencer Owen <owenspencer@gmail.com>
2019-04-07 22:23:33 -0600

    premature optimization not hard coding reqId

--- sandbox/nodewebsocket1/app.js
+++ sandbox/nodewebsocket1/app.js
@@ -10,7 +10,7 @@ const connection = new WebSocket(url);
 // Use requests library to get access_token
 var request = require('request');
 // var access_token = ''
-function getAccessToken() {
+function getAccessToken(fn) {
   request.post(
     'https://api.astroprint.com/v2/token',
     { json: {
@@ -21,14 +21,15 @@ function getAccessToken() {
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
           console.log(body);
-          return body.access_token;
+          fn(body.access_token);
       }
     }
   );
 }
 
-function sendToken (access_token) {
-  var foo = {"type": "response","reqId": "0","payload": {"accessToken": `${access_token}`}};
+function sendToken(reqId, access_token) {
+  var foo = {"type": "response","reqId": `${reqId}`,"payload": {"accessToken": `${access_token}`}};
+  foo = JSON.stringify(foo);
   console.log(`Sending token ${foo}`);
   connection.send(`${foo}`);
 }
@@ -36,7 +37,7 @@ function sendToken (access_token) {
 // var foo = {"type": "response","reqId": "0","payload": {"accessToken": `${access_token}`}}
 
 connection.onopen = function(){
-  getAccessToken(sendToken);
+
 }
 
 connection.onerror = error => {
@@ -44,5 +45,20 @@ connection.onerror = error => {
 }
 
 connection.onmessage = e => {
-  console.log(e.data)
+  console.log(e.data);
+  console.log(JSON.parse(e.data).type);
+  if (JSON.parse(e.data).type === "auth") {
+    console.log("got auth request");
+    getAccessToken(function(access_token){
+      console.log(JSON.stringify(access_token));
+      sendToken(JSON.parse(e.data).reqId, access_token);
+    });
+  }
+
+
+}
+
+// TODO: I made this up, is this correct?
+connection.onclose = () => {
+  console.log('disconnected');
 }
